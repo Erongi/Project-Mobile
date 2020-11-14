0c77148283bfc9bd346b017d082c6bb1723c3f40
Erongi <40891924+Erongi@users.noreply.github.com>
2020-11-14 16:33:16 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,9 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import Vibration from "./GameScreen/vibration";
-import color from "./GameScreen/color";
-import sound from "./GameScreen/sound";
-// import Math from "./GameScreen/math";
+import Color from "./GameScreen/color";
+import Sound from "./GameScreen/sound";
 
 export default function App() {
   return <Vibration />;

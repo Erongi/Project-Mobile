6faf6f55b5177b5be5cc066d6932d6d60023c645
Erongi <40891924+Erongi@users.noreply.github.com>
2020-11-14 16:30:32 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import Vibration from "./GameScreen/vibration";
+import color from "./GameScreen/color";
+import sound from "./GameScreen/sound";
+// import Math from "./GameScreen/math";
+
+export default function App() {
+  return <Vibration />;
+}
